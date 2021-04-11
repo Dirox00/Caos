15eae056bcfd562bc0359ac8cf61a6cbd45b6d1f
Dirox00 <dirox@mailo.com>
2021-04-11 20:23:57 +0200

    Coordinates

---
+++ Mandelbrot.py
@@ -0,0 +1,75 @@
+from PIL import Image, ImageDraw
+
+import math
+
+import time
+
+
+class Mandelbrot:
+    def __init__(self, region):
+        self.its = 50
+        self.size = (600, 600)
+        self.region = region
+        # self.region = (-2, 2, 2, -2)
+        # self.coords = (0, 0, 800, 800)
+
+        # self.x_interval = abs(self.region[0]) + abs(self.region[2])
+        # self.y_interval = abs(self.region[1]) + abs(self.region[3])
+
+    def zoom(self, coords):
+        """
+        coords: (x0, y0, +x, +y)
+        """
+        new_x0 = self.region[0] + coords[0]*self.region[2]/self.size[0]
+        new_y0 = self.region[1] - coords[1]*self.region[3]/self.size[1]
+        new_x_interval = coords[2]*self.region[2]/self.size[0]
+        new_y_interval = coords[3]*self.region[3]/self.size[1]
+
+        return (new_x0, new_y0, new_x_interval, new_y_interval)
+        
+    
+    def get_color(self, it):
+        r = round(255 * it / self.its)
+        
+        return 255, r, 0
+
+    def in_mandelbrot(self, x, y):
+        c = complex(x, y)
+        z = 0
+
+        for i in range(self.its):
+            if abs(z) > 2:
+                return False, i
+            z = z**2 + c
+        
+        return True, self.its  
+
+    def new_mandelbrot(self):
+        width, height = self.size
+
+        img = Image.new('RGB', self.size)
+
+        for x in range(width):
+            for y in range(height):
+                state, it = self.in_mandelbrot(self.region[0]+x*self.region[2]/width, self.region[1]-y*self.region[3]/height)
+                if state:
+                    img.putpixel((x, y), (0, 0, 0))
+                    # pixels[x, y] = (0, 0, 0)
+                else:
+                    img.putpixel((x, y), self.get_color(it))
+                    # pixels[x, y] = self.get_color(it)
+        
+        img.show()
+                
+
+mand = Mandelbrot((-2, 2, 4, 4))
+# mand.new_mandelbrot()
+
+new = mand.zoom((323, 194, 47, 47))
+print(new)
+m2 = Mandelbrot(new)
+
+new2 = m2.zoom((375, 103, 40, 40))
+print(new2)
+m3 = Mandelbrot(new2)
+m3.new_mandelbrot()
--- Mandelbrot_image.py
+++ Mandelbrot_image.py
@@ -4,7 +4,11 @@ import math
 
 def it_to_color(it, its):
     r = round(255 * it / its)
-    return 0, r, r
+    # return 255, r, 0 # reds
+    # return r, 0, r # purples
+    # return 0, r, r # blues
+    return 255, r, 0
+
 
 def in_mandelbrot(x, y, its):
     c = complex(x, y)
@@ -15,7 +19,8 @@ def in_mandelbrot(x, y, its):
             return False, i
         z = z**2 + c
     
-    return True, its    
+    return True, its  
+
 
 def new_mandelbrot(its=50, zoom=200, x_move=-400, y_move=-400, width=800, height=800):
     img = Image.new('RGB', (width, height))
@@ -24,7 +29,8 @@ def new_mandelbrot(its=50, zoom=200, x_move=-400, y_move=-400, width=800, height
     for x in range(img.size[0]):
         print("{:.2f} %".format(x / width * 100.0))
         for y in range(img.size[1]):
-            state, it = in_mandelbrot((x+x_move)/zoom,(y+y_move)/zoom, its)
+            # state, it = in_mandelbrot((x+x_move)/zoom,(y+y_move)/zoom, its)
+            state, it = in_mandelbrot(-2+x*4/width, 2-y*4/height, its)
             if state:
                 pixels[x, y] = (0, 0, 0)
             else:
@@ -35,6 +41,31 @@ def new_mandelbrot(its=50, zoom=200, x_move=-400, y_move=-400, width=800, height
 
     img.show()
 
-# new_mandelbrot()
-# new_mandelbrot(500, 10000, -1400, -9500)
-new_mandelbrot(100, 300, -600, -500, width=1920, height=1080)
+# new_mandelbrot(*convert_coords(50, 269, 205, 98, 311, 200, -400, -400, 800, 800))
+new_mandelbrot(its=30, zoom=200)
+
+
+# del otro programa:
+    # def new_mandelbrot(self):
+    #     img = Image.new('RGB', (self.width, self.height))
+
+    #     x_dif = self.width // 2
+    #     y_dif = self.height // 2
+
+    #     z_values = [[0]*self.width]*self.height
+
+    #     for i in range(self.its):
+    #         for x in range(self.width):
+    #             for y in range(self.height):
+    #                 if img.getpixel((x,y)) == (0,0,0):
+    #                     # c = complex((x-x_dif)/200, (y-y_dif)/200)
+    #                     # print(c)
+    #                     c = complex(-2+x*4/self.width, 2-y*4/self.height)
+    #                     z = z_values[y][x]
+    #                     # print(z)
+    #                     if abs(z) > 2:
+    #                         img.putpixel((x, y), self.get_color(i))
+    #                     else:
+    #                         z_values[y][x] = z ** 2 + c  # A lo mejor el **2 se puede mejorar
+    #         img.show()
+    #         time.sleep(2)
